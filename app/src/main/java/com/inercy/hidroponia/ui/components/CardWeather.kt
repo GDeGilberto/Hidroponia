b155fbf2d7e2488c30833f4467e736e5b7757e5b
Gilberto Alvarez <gilbertoalvarez514@hotmail.com>
2025-03-25 11:53:26 -0700

    A new CardWeather component is created to display weather information

---
+++ app/src/main/java/com/inercy/hidroponia/ui/components/CardWeather.kt
@@ -0,0 +1,197 @@
+package com.inercy.hidroponia.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.inercy.hidroponia.R
+import com.inercy.hidroponia.ui.theme.*
+
+@Preview
+@Composable
+fun CardWeather(
+    country: String = "Mexicali, B.C.",
+    date: String = "11 Mar | 12:56",
+    temp: Int = 23,
+    minTemp: Int = 18,
+    maxTemp: Int = 25,
+    weather: String = "Lluvia ligera"
+) {
+    var isDetailsVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(elevation = 8.dp)
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(BlueDarker, BlueBase2, TealSubtle)
+                ),
+                shape = RoundedCornerShape(8.dp)
+            ))
+    {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = country,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth(),
+                color = LightBase
+            )
+            Text(
+                text = date,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.fillMaxWidth(),
+                color = LightBase
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Row {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = "${temp}°",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 64.sp,
+                            color = LightBase
+                        )
+                        Text(
+                            text = "C",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 32.sp,
+                            color = LightBase
+                        )
+                    }
+
+                    Text(
+                        text = "Min ${minTemp}°C, Max ${maxTemp}°C",
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.fillMaxWidth(),
+                        color = LightBase
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lluvia),
+                        contentDescription = null,
+                        modifier = Modifier.size(72.dp)
+                    )
+                    Text(
+                        text = weather,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = LightBase
+                    )
+                }
+            }
+        }
+
+        if(isDetailsVisible){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(LightBase)
+                    .padding(horizontal = 8.dp, vertical = 10.dp)
+                    .horizontalScroll(rememberScrollState())
+            ) {
+                repeat(12) {
+                    ItemWeather()
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(
+                    shape = RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp)
+                )
+                .background(LightBase)
+                .clickable { isDetailsVisible = !isDetailsVisible }
+                .padding(top = 16.dp, bottom = 24.dp),
+            Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.ver_mas),
+                color = BlueDarker2
+            )
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                contentDescription = null,
+                modifier = Modifier.size(16.dp),
+                tint = BlueDarker2
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ItemWeather(
+    time: String = "00:00",
+    humanity: Int = 0,
+    temp: Int = 0
+) {
+    Column(
+        modifier = Modifier.padding(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Text(
+            text = time,
+            style = MaterialTheme.typography.bodyMedium,
+            color = DarkDarker
+        )
+        Text(
+            text = "${humanity}%",
+            style = MaterialTheme.typography.bodySmall,
+            color = BlueDarker
+        )
+        Image(
+            painter = painterResource(R.drawable.lluvia),
+            contentDescription = null,
+            modifier = Modifier.size(32.dp)
+        )
+        Text(
+            text = "${temp}°C",
+            style = MaterialTheme.typography.bodySmall,
+            color = DarkDarker
+        )
+    }
+}
--- app/src/main/java/com/inercy/hidroponia/ui/theme/Color.kt
+++ app/src/main/java/com/inercy/hidroponia/ui/theme/Color.kt
@@ -6,16 +6,92 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primary = Color(0xFF469D2B)//
+val primaryContainer = Color(0xFFB7F1B9)//
+val onPrimaryContainer = Color(0xFF2D4E30)//
+
+val secondary = Color(0xFFFFA79B)//
+val onSecondary = Color(0xFF561E18)//
+val secondaryContainer = Color(0xFFF1B7B7)//
+
+val tertiary = Color(0xFF3a666d)
+val tertiaryContainer = Color(0xFFBAE8F0)
+val onTertiaryContainer = Color(0xFFD0BCFF)//
+
+val surface = Color(0xFFF7FBF2)//
+val onSurface = Color(0xFF181D18)//
+
+val outline = Color(0xFF717970)//
+/////////////////////////////
+
+val GreenDarker = Color(0xFF469D2B)
+val GreenBase = Color(0xFFB7F1B9)
+val GreenSubtle = Color(0xFFE8EDE5)
+
+val BlueDarker = Color(0xFF3568D4)
+val BlueBase = Color(0xFF6698FA)
+val BlueSubtle = Color(0xFFE4F1FF)
+
+val RedDarker = Color(0xFFFF3B3B)
+val RedBase = Color(0xFFFF8080)
+val RedSubtle = Color(0xFFFFE9EB)
+
+val OrangeDarker = Color(0xFFFF8800)
+val OrangeBase = Color(0xFFFCCC75)
+val OrangeSubtle = Color(0xFFFFF8E5)
+
+val YellowDarker = Color(0xFFFFCC00)
+val YellowBase = Color(0xFFFDED72)
+val YellowSubtle = Color(0xFFFFFEE5)
+
+val TealDarker = Color(0xFF00CFDE)
+val TealBase = Color(0xFFA9EFF2)
+val TealSubtle = Color(0xFFE5FFFF)
+
+val PurpleDarker = Color(0xFF6600CC)
+val PurpleBase = Color(0xFFDDA5E9)
+val PurpleSubtle = Color(0xFFFFE5FF)
+
+val DarkDarker = Color(0xFF28293D)
+val DarkBase = Color(0xFF555770)
+val DarkLight = Color(0xFF8F90A6)
+
+val GreyBase = Color(0xFFC7C9D9)
+val GreySubtle  = Color(0xFFEBEBF0)
+
+val LightDarker = Color(0xFFF2F2F5)
+val LightBase = Color(0xFFFAFAFC)
+val LightSubtle = Color(0xFFFFFFFF)
 
 
-val YellowLight = Color(0xFFFEF8E8)
-val YellowStrong = Color(0xFFF6B516)
+val RedDarker2 = Color(0xFFE53535)
+val RedBase2 = Color(0xFFFF3B3B)
+val RedSubtle2 = Color(0xFFFF5C5C)
 
-val RedLight = Color(0xFFFFE9EB)
-val RedStrong = Color(0xFFEB5965)
+val BlueDarker2 = Color(0xFF3568d4)
+val BlueBase2 = Color(0xFF3E7BFA)
+val BlueSubtle2 = Color(0xFF6698fa)
 
-val BlueLight = Color(0xFFE4F1FF)
-val BlueStrong = Color(0xFF4389DC)
+val GreenDarker2 = Color(0xFF05a660)
+val GreenBase2 = Color(0xFF06c270)
+val GreenSubtle2 = Color(0xFF39d98a)
+
+val OrangeDarker2 = Color(0xFFe57a00)
+val OrangeBase2 = Color(0xFFff8800)
+val OrangeSubtle2 = Color(0xFFfdac42)
+
+val YellowDarker2 = Color(0xFFe5b800)
+val YellowBase2 = Color(0xFFffcc00)
+val YellowSubtle2 = Color(0xFFfddd48)
+
+val TealDarker2 = Color(0xFF00b7c4)
+val TealBase2 = Color(0xFF00fcde)
+val TealSubtle2 = Color(0xFF73dfe7)
+
+val PurpleDarker2 = Color(0xFF4d0099)
+val PurpleBase2 = Color(0xFF6600cc)
+val PurpleSubtle2 = Color(0xFFac5dd9)
+
+val LightDarker2 = Color(0xFFebebf0)
+val LightBase2 = Color(0xFFf2f2f5)
+val LightSubtle2 = Color(0xFFfafafc)

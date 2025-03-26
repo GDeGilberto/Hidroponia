903d1d6d91c2c5ee2fb8d1918a0f6ab50ef1db92
Gilberto Alvarez <gilbertoalvarez514@hotmail.com>
2025-03-26 16:27:53 -0700

    A new BarProgress component is created to display the evaluated state with the limits.

---
+++ app/src/main/java/com/inercy/hidroponia/ui/components/BarProgress.kt
@@ -0,0 +1,153 @@
+package com.inercy.hidroponia.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.inercy.hidroponia.R
+import com.inercy.hidroponia.ui.models.StatusEnum
+
+/**
+ * @param iconLabel Path of the drawable for the icon accompanying the label
+ * @param color The color to tint the icon accompanying the label
+ * @param value Data corresponding to the represented property
+ * @param min Minimum tolerated value
+ * @param max Maximum tolerated value
+ */
+@Preview(showBackground = true)
+@Composable
+fun BarProgress(
+    @DrawableRes iconLabel: Int = R.drawable.sun,
+    label: String = stringResource(R.string.title),
+    color: Color = Color.Black,
+    value: Float = 0f,
+    min: Float = 0f,
+    max: Float = 100f,
+    modifier: Modifier = Modifier
+) {
+    val status = when {
+        value == 0f -> StatusEnum.Information
+        value <= min + (max - min) * 0.10f || (value < min || value > max) -> StatusEnum.Error
+        value <= min + (max - min) * 0.20f -> StatusEnum.Warning
+        else -> StatusEnum.OK
+    }
+
+    Row(modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(modifier = modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Row(modifier = modifier.padding(horizontal = 4.dp)) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = iconLabel),
+                        tint = color,
+                        contentDescription = null,
+                        modifier = modifier.size(16.dp))
+                    Text(
+                        text = label,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Row(modifier = modifier.weight(1f),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    InformationText(
+                        R.drawable.bottom,
+                        label = stringResource(R.string.min),
+                        value = min.toInt()
+                    )
+                    Spacer(modifier = modifier.width(8.dp))
+                    InformationText(
+                        R.drawable.top,
+                        label = stringResource(R.string.max),
+                        value = max.toInt()
+                    )
+                }
+            }
+            BarSimple(value, min, max, color = status.color, colorLight = status.colorLight)
+        }
+
+        Column(modifier = modifier.padding(start = 8.dp)) {
+            Icon(
+                imageVector = status.icon,
+                contentDescription = null,
+                tint = status.color,
+                modifier = modifier.size(20.dp)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun BarSimple(
+    value: Float,
+    min: Float,
+    max: Float,
+    color: Color,
+    colorLight: Color = Color.LightGray,
+    modifier: Modifier = Modifier
+) {
+    val progress = (value - min).coerceIn(0f, max - min) / (max - min) // Normalization progress
+
+    //surface
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(colorLight)
+    ) {
+        //bar color
+        Box(
+            modifier = modifier
+                .fillMaxHeight()
+                .fillMaxWidth(fraction = progress)
+                .clip(RoundedCornerShape(16.dp))
+                .background(color)
+        )
+    }
+}
+
+
+@Composable
+fun InformationText(
+    @DrawableRes icon: Int,
+    label: String,
+    value: Int
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = null
+        )
+        Text(text = "$label: $value")
+    }
+}
---
+++ app/src/main/java/com/inercy/hidroponia/ui/models/StatusEnum.kt
@@ -0,0 +1,17 @@
+package com.inercy.hidroponia.ui.models
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.inercy.hidroponia.ui.theme.*
+
+enum class StatusEnum(val id: Int, val color: Color, val colorLight: Color, val icon: ImageVector) {
+    OK(id = 1, color = GreenBase2, colorLight = GreenSubtle, icon = Icons.Filled.CheckCircle),
+    Warning(id = 2, color = YellowDarker, colorLight = YellowSubtle, icon = Icons.Filled.Warning),
+    Error(id = 3, color = RedDarker, colorLight = RedSubtle, icon = Icons.Filled.Close),
+    Information(id = 4, color = BlueDarker, colorLight = BlueSubtle, icon = Icons.Filled.Info),
+}

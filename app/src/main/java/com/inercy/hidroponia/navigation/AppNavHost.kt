8281554f3b2e39cd6faca8c6569799b76637e5c4
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-04-10 16:45:20 -0700

    "The screen is updated when the app starts, and a new route is added indicating the new LoginScreen

--- app/src/main/java/com/inercy/hidroponia/navigation/AppNavHost.kt
+++ app/src/main/java/com/inercy/hidroponia/navigation/AppNavHost.kt
@@ -6,24 +6,32 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.inercy.hidroponia.ui.screens.GreenHouseScreen
+import com.inercy.hidroponia.ui.screens.LoginScreen
 import com.inercy.hidroponia.ui.screens.home.HidroponiaHomeScreen
 
 /**
- * NavGraph realiza la asignacion de los destinos composable a los que se navegara
- * @param navController Clase con el objetivo de navegar entre pantallas
- * @param startDestination Es una ruta de predeterminada cuando la app muestra por primera vez
+ * NavGraph assigns the composable destinations to be navigated.
+ * @param navController "Class designed for screen navigation
+ * @param startDestination It is a default route when the app is displayed for the first time
  */
 @Composable
 fun AppNavHost(
     navController: NavHostController,
-    startDestination: String = Home.route,
+    startDestination: String = Login.route,
     modifier: Modifier = Modifier
 ) {
     NavHost(
         navController = navController,
-        startDestination = Home.route,
+        startDestination = startDestination,
         modifier = modifier
     ) {
+        composable(route = Login.route) {
+            LoginScreen(
+                onLoginClick = { _, _ ->
+                    navController.navigate(Home.route)
+                }
+            )
+        }
         composable(route = Home.route) {
             HidroponiaHomeScreen(
                 navigateTo = { navController.navigate(GreenHouse.route) },
